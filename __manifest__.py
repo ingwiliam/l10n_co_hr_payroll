c5029bd3e116589e57e178c34c9e2ad306d46d1d
William Peña <ingwilliamconsultor@gmail.com>
2021-02-18 08:37:42 -0500

    ADD l10n_co_hr_payroll

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Nómina Colombiana',
+    'category': 'Human Resources',
+    'depends': ['hr_payroll'],
+    "author"  :  "William Peña",
+  "license"   :  "Other proprietary",
+  "website"   :  "https://www.ogsistemas.com",
+    'description': """
+Nomina Colombiana
+======================
+
+* Nómina Básica Colombiana
+    """,
+
+    'data': [
+        'views/l10n_co_hr_payroll_view.xml',
+        'data/l10n_co_hr_payroll_data.xml',
+    ],
+}
